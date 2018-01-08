51289b5964984364e1ab559886efd302629055bc
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2018-01-08 14:48:50 +0100

    Remove load balancer from hosts files

--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -7,7 +7,6 @@ sudo echo "
 192.168.50.2 k8s-master
 192.168.50.3 k8s-worker-0
 192.168.50.4 k8s-worker-1
-192.168.50.1 k8s-lb
 " >> /etc/hosts
 
 sysctl net.bridge.bridge-nf-call-iptables=1
@@ -16,7 +15,7 @@ sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+cat > /etc/apt/sources.list.d/kubernetes.list <<EOF
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
@@ -24,4 +23,3 @@ apt-get install -y kubelet kubeadm kubectl
 
 systemctl enable kubelet
 systemctl enable docker
-
